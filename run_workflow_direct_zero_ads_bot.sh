2a6b07750e5087cdd42a3d4668d93d3ca609c3f5
PureMagic808 <amfitesla19@gmail.com>
2025-03-31 20:06:23 +0500

    Delete run_workflow_direct_zero_ads_bot.sh

--- run_workflow_direct_zero_ads_bot.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-# Прямой запуск Telegram-бота с нулевой толерантностью к рекламе через workflow run_bot
-echo "Запуск Telegram-бота с нулевой толерантностью к рекламе..."
-export REPLIT_WORKFLOW="run_bot"
-python run_direct_zero_ads_bot.py
